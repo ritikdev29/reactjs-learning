72482ed4058aa63118fad4791d076c51ccfd721c
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-07-31 19:35:18 +0530

    Components

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Card from './components/card'
+import Navbar from './components/Navbar';
+
+const user = 'Hi, Gautam  '
+let r = 20;
+let g = 40;
+let result = r + g;
+
+let age = 21;
+
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <div className='card'>
-        <h1>Ritik Kumar </h1>
+        <h1>Ritik Kumar  {user}{result}</h1>
+        <h2>And I Am  {age} Years Old</h2>
         {<p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci sit error voluptas laudantium culpa, dolores qui deleniti corrupti, aspernatur nostrum tempora! Mollitia ipsum autem in recusandae molestias possimus enim laboriosam.</p>}
 
       </div>
--- 02-components/src/components/Navbar.jsx
+++ 02-components/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const card = () => {
+const Navbar = () => {
     return (
         <div>
 
@@ -8,4 +8,4 @@ const card = () => {
     )
 }
 
-export default card
+export default Navbar
