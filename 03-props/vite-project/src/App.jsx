7f7accc290ab2175c7c560d3bfe55604c94f7646
Ritik Kumar Gupta <ririkkumargupta@gmail.com>
2026-08-03 12:37:58 +0530

    Project Pros

--- 03-props/vite-project/src/App.jsx
+++ 03-props/vite-project/src/App.jsx
@@ -6,8 +6,10 @@ const App = () => {
     <div className='parent'>
       <Card user="Gautam" age={18} img="https://plus.unsplash.com/premium_photo-1770876284693-21ae221757ed?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       <Card user="Ritik Kumar" age={20} img="https://plus.unsplash.com/premium_photo-1770876284693-21ae221757ed?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <Card user="Adity Rao" age={22} img="https://plus.unsplash.com/premium_photo-1783394326767-5104b53dba6e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
     </div>
   )
 }
 
 export default App
+
