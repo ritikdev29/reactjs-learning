c2bcbe2f731c576a4ab7ed8c0f52210be46d96be
ritikdev29 <ririkkumargupta@gmail.com>
2026-08-18 20:10:04 +0530

    add

--- 09-useState/src/App.jsx
+++ 09-useState/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 
 const App = () => {
+  const [num, setNum] = useState(20)
+
+  function changeNum() {
+    setNum(90)
+    // console.log('hi'); for update value or increase values number...
+
+
+
+  }
+
+  // let a = 10;
+  // function changeA() {
+  //   console.log(a);
+  //   a++;
+  //   console.log(a);
+  // }
   return (
     <div>
+      <h1> Value of a is {num}</h1>
+      {/* <button onClick={changeA}>Click</button> */}
 
+      <button onClick={changeNum}>Click</button>
     </div>
   )
 }
